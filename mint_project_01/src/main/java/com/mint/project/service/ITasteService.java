5165d5341219a65da05531e5849c34f179372d8e
Administrator <Administrator@PURE073>
2018-08-26 12:21:07 +0900

    08.26 mapper, dao, service / taste, genre

--- mint_project_01/src/main/java/com/mint/project/service/ITasteService.java
+++ mint_project_01/src/main/java/com/mint/project/service/ITasteService.java
@@ -1,21 +1,25 @@
 package com.mint.project.service;
 
+import java.util.List;
+
 import com.mint.project.dtos.TasteDto;
 
 public interface ITasteService {
 
 	
 	//초기 취향정보 생성
-	public int insertTaste(TasteDto tdto);
+	public boolean insertTaste(TasteDto tdto);
 	
 	//취향정보 가져옴
 	public TasteDto getTaste(int useq);
+	
+	public List<TasteDto> getAllTaste();
 	
 	//취향정보 수정
 	public boolean updateTaste(TasteDto tdto);
 	
 	//취향정보 삭제
-	public boolean deleteTaste(int useq);
+	public boolean delTaste(int useq);
 	
 	
 	
--- mint_project_01/src/main/java/com/mint/project/service/TasteServiceImp.java
+++ mint_project_01/src/main/java/com/mint/project/service/TasteServiceImp.java
@@ -1,34 +1,45 @@
 package com.mint.project.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mint.project.daos.ITasteDao;
 import com.mint.project.dtos.TasteDto;
 
 @Service
 public class TasteServiceImp implements ITasteService {
+	
+	@Autowired
+	private ITasteDao tdao;
 
 	@Override
-	public int insertTaste(TasteDto tdto) {
-		// TODO Auto-generated method stub
-		return 0;
+	public boolean insertTaste(TasteDto tdto) {
+		return tdao.insertTaste(tdto);
 	}
 
 	@Override
 	public TasteDto getTaste(int useq) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return tdao.getTaste(useq);
+	}
+	
+	@Override
+	public List<TasteDto> getAllTaste(){
+		return tdao.getAllTaste();
 	}
 
 	@Override
 	public boolean updateTaste(TasteDto tdto) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return tdao.updateTaste(tdto);
 	}
 
 	@Override
-	public boolean deleteTaste(int useq) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean delTaste(int useq) {
+		
+		return tdao.delTaste(useq);
 	}
 
 }
