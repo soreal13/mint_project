bd52b10177d6a52a9e3c43578e26e3c8a9892b4b
sojin13 <41860917+sojin13@users.noreply.github.com>
2018-08-27 09:48:04 +0900

    Revert "8/27 sj first merge test"

--- mint_project_01/src/main/java/com/mint/project/service/ITasteService.java
+++ mint_project_01/src/main/java/com/mint/project/service/ITasteService.java
@@ -1,25 +1,21 @@
 package com.mint.project.service;
 
-import java.util.List;
-
 import com.mint.project.dtos.TasteDto;
 
 public interface ITasteService {
 
 	
 	//초기 취향정보 생성
-	public boolean insertTaste(TasteDto tdto);
+	public int insertTaste(TasteDto tdto);
 	
 	//취향정보 가져옴
 	public TasteDto getTaste(int useq);
-	
-	public List<TasteDto> getAllTaste();
 	
 	//취향정보 수정
 	public boolean updateTaste(TasteDto tdto);
 	
 	//취향정보 삭제
-	public boolean delTaste(int useq);
+	public boolean deleteTaste(int useq);
 	
 	
 	
